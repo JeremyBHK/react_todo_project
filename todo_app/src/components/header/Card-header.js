efa3b72042f8bd2bcbfa0d4a939dbfb60a8ff6e6
Jérémy Blahak <jeremyblahak@macbook-air-de-jeremy.home>
2019-07-18 01:02:21 +0200

    ajout header

--- todo_app/src/components/header/Card-header.js
+++ todo_app/src/components/header/Card-header.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import Button from '../button/Button';
 import Arrow from '../../static/img/V.svg';
+import TaskIlu from '../../static/img/Taskilu.png';
 
 class CardHeader extends Component{
     render(){
         return(
             <div id="card-header">
-                <img src="" alt=""></img>
-                <p>Ici celles que vous avez <br/>acceptées</p>
-                <Button>
-                    <div className="circle"><p>1</p></div>
-                    <p>Tâches</p>
-                    <img src={Arrow} alt="Arrow"/>
-                </Button>
+                <img id="TaskIlu" src={TaskIlu} alt="TaskIlu"></img>
+                <div>
+                    <p>Ici celles que vous avez <br/>acceptées</p>
+                    <Button>
+                        <div className="circle"><p>3</p></div>
+                        <p>Tâches</p>
+                        <img src={Arrow} alt="Arrow"/>
+                    </Button>
+                </div>
             </div>
         )
     }
