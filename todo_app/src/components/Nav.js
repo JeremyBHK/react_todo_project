2e43f02e758f0fc438496e6009de75ec7a733ef3
Kahn-Barberon Valentin <kahnba_v16@myges.fr>
2019-07-18 11:23:50 +0200

    nav

---
+++ todo_app/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import backgroundNav from '../../static/img/background-nav.png';
+
+const Nav = () => {
+  return(
+    <nav>
+      <ul>
+        <li><a><img src="" alt="Tâche icon" /></a></li>
+        <li><a><img src="" alt="Tâche icon" /></a></li>
+        <li><a><img src="" alt="Tâche icon" /></a></li>
+        <li><a><img src="" alt="Tâche icon" /></a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
