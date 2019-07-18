2da3fe0c15a0f4f8df06969e6eb10c21a5e1cd59
Jérémy Blahak <jeremyblahak@macbook-air-de-jeremy.home>
2019-07-19 01:25:43 +0200

    component task add

--- todo_app/src/components/Home.js
+++ todo_app/src/components/Home.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'; 
 
 import Header from './header/Header';
+import Task from './tasks/Task';
 
 class Home extends Component{
     render(){
         return(
             <React.Fragment>
                 <Header name="Bonjour Benjamin" p="Voici toutes les tâches que" p2="vous pouvez réaliser"/>
-                <Link to="/tasks" title="Lien vers la page Tasks">
-                    Lien vers tasks
-                </Link>
+                <h2>Les tâches urgentes</h2>
+                <section>
+                    <Task />
+                </section>
+                {/* <Link to="/tasks" title="Lien vers la page Tasks">
+                    Lien vers la page des tâches
+                </Link> */}
             </React.Fragment>
         )
     }
---
+++ todo_app/src/components/tasks/Task.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Urgent from '../../static/img/urgentes.svg';
+import Pin from '../../static/img/Subtract.svg';
+
+class Task extends Component{
+    render(){
+        return(
+            <article id="component-task">
+                <div className="img-task">
+                    <p className="price-task">7€</p>
+                </div>
+                <div>
+                    <p>15 juin <br/>12h30</p>
+                    <div>
+                        <img src={Pin} alt="Pin de localisation"/>
+                        <p>À 5 minutes</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Durée <br/><span>40min</span></p>
+                    <img src={Urgent} alt="icon urgent" />
+                </div>
+            </article>
+        )
+    }
+}
+
+export default Task
