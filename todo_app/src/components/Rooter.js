d22a4c7b54322073bcc4babb5114d7cb1713fb4b
Kahn-Barberon Valentin <kahnba_v16@myges.fr>
2019-07-20 22:45:16 +0200

    ad header 2

--- todo_app/src/components/Rooter.js
+++ todo_app/src/components/Rooter.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './Home';
-import Tasks from './tasks/Tasks';
-import Admin from './admin/Admin';
-import UrgentTask from './tasks/UrgentTask';
-import RecurTask from './tasks/RecurTask';
-import PrevTask from './tasks/PrevTask';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from './Home'
+import Tasks from './tasks/Tasks'
+import Admin from './admin/Admin'
+import UrgentTask from './tasks/UrgentTask'
+import RecurTask from './tasks/RecurTask'
+import PrevTask from './tasks/PrevTask'
+import LayoutHeader from '../components/header/LayoutHeader'
 
 class Rooter extends Component{
     render(){
@@ -14,7 +15,7 @@ class Rooter extends Component{
                 <Route exact path="/" component={Home}/>
                 <Route path="/tasks" component={Tasks}/>
                 <Route path="/admin" component={Admin}/>
-                <Route path="/accept-task" component={Admin}/>
+                <Route path="/accept-task" component={LayoutHeader}/>
                 <Route path="/urgent-tasks" component={UrgentTask} />
                 <Route path="/recurrent-tasks" component={RecurTask} />
                 <Route path="/forecast-tasks" component={PrevTask} />
--- todo_app/src/components/header/LayoutHeader.js
+++ todo_app/src/components/header/LayoutHeader.js
@@ -1,12 +1,24 @@
-import React from ' react'
-import Back from '../../static/img/header-2.svg'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Arrow from '../../static/img/arrow-toleft.svg'
 
 const LayoutHeader = () => {
-  return (
-    <React.Fragment>
-      <header>
-        
-      </header>
-    </React.Fragment>
-  )
+    return (
+      <React.Fragment>
+        <header className="LayoutHeader">
+          <div className="layoutTitle">
+          <Link to="/" title="Lien vers la page Home">
+          <img src={Arrow} alt="Arrow to left" />
+          </Link>
+            <h1>Bouquet de fleurs</h1>
+          </div>
+          <div className="layoutContent">
+            <p>15 Juin 2019</p>
+            <p className="rounded">12:30</p>
+          </div>
+        </header>
+      </React.Fragment>
+    )
 }
+
+export default LayoutHeader
