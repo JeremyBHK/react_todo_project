49c5053f8b93bc1bbe0d26aa68413bf70bbff448
Kahn-Barberon Valentin <kahnba_v16@myges.fr>
2019-07-20 02:45:44 +0200

    remove useless files

--- todo_app/src/components/admin/Admin.js
+++ todo_app/src/components/admin/Admin.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import { HydraAdmin } from '@api-platform/admin'
+import { createMuiTheme } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light', // Switching the dark mode on is a single property value change.
+  },
+});
 class App extends Component { 
   render() {
-    return <HydraAdmin entrypoint="http://127.0.0.1:8000/api/"/> // Replace with your own API entrypoint
+    return <HydraAdmin theme={theme} entrypoint="http://127.0.0.1:8000/api/"/> // Replace with your own API entrypoint
   }
 }
 
